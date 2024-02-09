9df340d6ea9aa3cda150520af9c415a8c97cbf75
MiliSegal <120659111+MiliSegal@users.noreply.github.com>
2024-02-09 16:59:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myrs/MyData.java
@@ -0,0 +1,25 @@
+package com.example.myrs;
+
+
+
+public class MyData {
+
+    static String[] nameArray = {"Bloom:", "Flora:", "Stella:", "Brandon:", "Riven:", "Sky:", "Icy:","Darcy:", "Stormy:", "Kiko:"};
+    static String[] versionArray = {
+            "The Fairy of the Dragon Flame. She has fire and heat-based powers. She is the most powerful fairy and the leader of the Winx Club.",
+            "The sensitive and shy Fairy of Nature from the planet Lynphea. Flora draws her strength from plants and her room at Alfea resembles a greenhouse.",
+            "The outgoing and spontaneous Fairy of the Shining Sun. She is the princess of Solaria.Her powers involve manipulating light and using energy from the sun and moon.",
+            "Sky's best friend and bodyguard, and Stella's boyfriend. He is the strongest of the Specialists and the most outgoing of the boys.",
+            "The competitive and stubborn maverick of the Specialists. His mom abandoned him at birth and he is wary of women. He has an intermittent relationship with Musa.",
+            "The leader of the Specialists. He is the crown prince and later king of the planet Eraklyon. He and Bloom date throughout the series.",
+            "The eldest of the trio and their leader, whose powers are derived from ice. She despises Bloom and is the most competitive with the Winx.",
+            "The middle of the trio, derives her powers from mind, shadows, and darkness. She enjoys tormenting her enemies and is less aggressive than her friends.",
+            "The youngest of the trio, has power over stormy weather. She is the most hot-tempered of the group and is prone to violent outbursts. Her impulsiveness often leads the trio to trouble.",
+            "Bloom's pet bunny-rabbit. He has blue fur with white splashes. He is highly intelligent but no one else seems to notice. He is able to pass through the magic barrier that blocks outsiders from Alfea."};
+
+    static Integer[] drawableArray = {R.drawable.bloom, R.drawable.flora, R.drawable.stella,
+            R.drawable.brandon, R.drawable.riven, R.drawable.skyyy, R.drawable.icyy,
+            R.drawable.darcyy, R.drawable.stormy, R.drawable.kiko};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+}
